8bdb323ba6c4336b89c37a68da388aa925dafa1e
최재경 <vmflsxj0@naver.com>
2020-07-04 11:33:52 +0900

    DbContext 추가

---
+++ StudyWebApplication/DbContext/OracleDataContext.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Oracle.ManagedDataAccess.Client;
+
+namespace StudyWebApplication.DbContext
+{
+    public class OracleDataContext
+    {
+        private static OracleConnection Conn { get; set; } = new OracleConnection("Data Source = orcl; User Id = cjk; Password = nvr9rud9");
+
+        private static bool OracleConnect()
+        {
+            try
+            {
+                Conn.Open();
+            }
+            catch (Exception)
+            {
+                Conn.Close();
+                
+                return false;
+            }
+            
+            return true;
+        }
+
+        public static DataSet ExecuteDataSet(StringBuilder strSql)
+        {
+            if (OracleConnect() == false)
+            {
+                return null;
+            }
+            
+            using DataSet dataSet = new DataSet();
+            using OracleDataAdapter adapter = new OracleDataAdapter()
+            {
+                SelectCommand = new OracleCommand(strSql.ToString(), Conn)
+            };
+
+            adapter.Fill(dataSet);
+
+            Conn.Close();
+
+            return dataSet;
+        }
+    }
+}
